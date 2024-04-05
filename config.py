e8fa8f36cd834accd210c048ba9237723b06aefc
edwardsemisotov <79847432+edwardsemisotov@users.noreply.github.com>
2024-04-06 03:40:47 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-youtube_api = 'AIzaSyCxuRpwk0fYsi8_Usv472pYmn5LhjHV2V8'
-core_dbname = "coredb"
-core_user = "postgres"
-core_password = "mysecretpassword"
-core_host = "localhost"
-core_port = "5432"
-tg_bot_token = '6967417073:AAGkkgVcyc7i5uReF1ceWZEeB2LvtKzUDTk'
-channel_id = '@BelarusCatholicDigest'
+
